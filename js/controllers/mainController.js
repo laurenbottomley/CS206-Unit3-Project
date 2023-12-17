e11269a8f108b11b5bf6f9f30893bed56690524b
laurenbottomley <laurenbottomley@icloud.com>
2023-12-17 17:33:56 -0500

    logic

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,7 +1,12 @@
-pizzaApp.controller('mainController', function ($location) {
+pizzaApp.controller('mainController', function ($location, $http, $scope) {
     var vm = this;
 
     vm.isActive = function (destination) {
         return destination === $location.path();
     };
+
+    $http.get('data/announcements.json')
+        .then(function(response) {
+            $scope.announcements = response.data;
+        });
 });
