6c2295142117bcd1c9cfbb8dc46414d3c56af516
laurenbottomley <laurenbottomley@icloud.com>
2023-12-16 21:46:06 -0500

    order logic tbc

--- js/controllers/orderController.js
+++ js/controllers/orderController.js
@@ -1,69 +1,64 @@
 pizzaApp.controller('orderController', function ($scope) {
-    $scope.pageTitle = 'Place an Order (Pizza Only)';
-    $scope.userName = '';
-    $scope.email = '';
-    $scope.phone = '';
-    $scope.selectedBase = '';
-    $scope.toppings = [];
-    $scope.notes = '';
-    $scope.creditCard = '';
-    $scope.expDate = '';
-    $scope.cvv = '';
 
-    // Toppings list
-    $scope.availableToppings = [
-        'Pepperoni', 'Mushrooms', 'Sausage', 'Green Peppers', 'Onions', 'Tomatoes', 'Cheese', 'Bacon', 'Olives'
-    ];
-    $scope.premiumToppings = [
-        'Chicken', 'Steak', 'Shrimp', 'Anchovies', 'Spinach', 'Artichokes', 'Feta Cheese', 'Extra Cheese'
-    ];
+    $scope.pageTitle = 'Order Online (Pizza Only)';
 
-    $scope.addTopping = function (topping) {
-        if ($scope.toppings.indexOf(topping) === -1) {
-            $scope.toppings.push(topping);
-        }
-    }
-
-    $scope.removeTopping = function (topping) {
-        var index = $scope.toppings.indexOf(topping);
-        if (index > -1) {
-            $scope.toppings.splice(index, 1);
-        }
-    }
-
+    // Initialize toppings in $scope.order
+    $scope.order = {
+        userName: '',
+        email: '',
+        phone: '',
+        toppings: [],
+        premiumToppings: [],
+        pizzaBase: '',
+        notes: '',
+        creditCard: '',
+        expDate: '',
+        cvv: ''
+    };
+    
+        // Define toppings
+        $scope.toppings = ['Pepperoni','Anchovies','Sausage','Mushrooms', 'Peppers','Garlic', 'Extra Cheese', 'Ham', 'Broccoli', 'Pesto Sauce', 'Black Olives', 'Bacon', 'Ricotta', 'Spinach', 'Pineapple'];
+        $scope.premiumToppings = ['Chicken', 'Steak', 'Eggplant', 'Artichokes', 'Meatballs', 'Roasted Peppers'];
+        
     $scope.baseCost = 0;
-    $scope.toppingsCost = 0;
     $scope.totalCost = 0;
 
-    // Update costs when base or toppings change
-    $scope.$watchGroup(['selectedBase', 'toppings'], function () {
-        // Calculate base cost
-        if ($scope.selectedBase === 'Plain') {
+    $scope.$watchGroup(['order.pizzaBase'], function () {
+        if ($scope.order.pizzaBase === 'Plain') {
             $scope.baseCost = 10.00;
-        } else if ($scope.selectedBase === 'Gluten Free' || $scope.selectedBase === 'White') {
+        } else if ($scope.order.pizzaBase === 'Gluten Free' || $scope.order.pizzaBase === 'White') {
             $scope.baseCost = 12.00;
         }
 
-        // Calculate toppings cost
-        $scope.toppingsCost = $scope.toppings.length * 1.50;
-
         // Update total cost
-        $scope.totalCost = $scope.baseCost + $scope.toppingsCost;
+        $scope.totalCost = $scope.baseCost;
     });
+
+    // Calculate Total Cost function
+    $scope.calculateTotalCost = function () {
+        return $scope.baseCost;
+    };
 
     // Submit order function
     $scope.submitOrder = function () {
-        console.log('Order Submitted:', {
-            userName: $scope.userName,
-            email: $scope.email,
-            phone: $scope.phone,
-            selectedBase: $scope.selectedBase,
-            toppings: $scope.toppings,
-            notes: $scope.notes,
-            creditCard: $scope.creditCard,
-            expDate: $scope.expDate,
-            cvv: $scope.cvv,
-            totalCost: $scope.totalCost
-        });
+        console.log('Order Submitted:', $scope.order);
+        $scope.clearForm();
+        alert('Thank you for your order! Your pizza will be ready in 30 minutes.');
+    };
+
+    // Clear form function
+    $scope.clearForm = function () {
+        $scope.order = {
+            userName: '',
+            email: '',
+            phone: '',
+            pizzaBase: '',
+            notes: '',
+            creditCard: '',
+            expDate: '',
+            cvv: ''
+        };
+        $scope.baseCost = 0;
+        $scope.totalCost = 0;
     };
 });
