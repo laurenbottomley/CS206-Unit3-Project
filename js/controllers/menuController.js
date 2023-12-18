0aa6389b6d0b2140c0f70832a0ee531da34e398c
laurenbottomley <laurenbottomley@icloud.com>
2023-12-17 21:06:45 -0500

    filter and toggles

--- js/controllers/menuController.js
+++ js/controllers/menuController.js
@@ -1,6 +1,14 @@
 pizzaApp.controller('menuController', function ($scope, $http) {
     $http.get('/data/menu.json').then(function (response) {
         $scope.menu = response.data;
+
+        angular.forEach($scope.menu.categories, function (category) {
+            category.show = true; // Initially show all categories
+        });
     });
-}
-);
+
+    // Toggle the visibility of menu items
+    $scope.toggleCategory = function (category) {
+        category.show = !category.show;
+    };
+});
